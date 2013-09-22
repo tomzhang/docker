db52a33693d07506059267c4571fb3bb7b219f7a
Daniel Mizyrycki <daniel@dotcloud.com>
2013-09-21 22:10:17 -0700

    testing infrastructure, issue #1814: Add tox and S3 docker registry tests

--- testing/buildbot/setup_credentials.sh
+++ testing/buildbot/setup_credentials.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Setup of test credentials. Called by Vagrantfile
+# Setup of test credentials for index and registry. Called by Vagrantfile.
 export PATH="/bin:sbin:/usr/bin:/usr/sbin:/usr/local/bin"
 
 USER=$1
 REGISTRY_USER=$2
 REGISTRY_PWD=$3
+REGISTRY_BUCKET=$4
+REGISTRY_ACCESS_KEY=$5
+REGISTRY_SECRET_KEY=$6
 
 BUILDBOT_PATH="/data/buildbot"
 DOCKER_PATH="/go/src/github.com/dotcloud/docker"
@@ -15,3 +18,6 @@ function run { su $USER -c "$1"; }
 run "cp $DOCKER_PATH/testing/buildbot/credentials.cfg $BUILDBOT_PATH/master"
 cd $BUILDBOT_PATH/master
 run "sed -i -E 's#(export DOCKER_CREDS=).+#\1\"$REGISTRY_USER:$REGISTRY_PWD\"#' credentials.cfg"
+run "sed -i -E 's#(export S3_BUCKET=).+#\1\"$REGISTRY_BUCKET\"#' credentials.cfg"
+run "sed -i -E 's#(export S3_ACCESS_KEY=).+#\1\"$REGISTRY_ACCESS_KEY\"#' credentials.cfg"
+run "sed -i -E 's#(export S3_SECRET_KEY=).+#\1\"$REGISTRY_SECRET_KEY\"#' credentials.cfg"
--- testing/functionaltests/test_registry.sh
+++ testing/functionaltests/test_registry.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
+set -x
+
 # Cleanup
 rm -rf docker-registry
 
-# Get latest docker registry
-git clone https://github.com/dotcloud/docker-registry.git
+# Setup the environment
+export SETTINGS_FLAVOR=test
+export DOCKER_REGISTRY_CONFIG=config_test.yml
 
-# Configure and run registry tests
-cd docker-registry; cp config_sample.yml config.yml
-cd test; python -m unittest workflow
+# Get latest docker registry
+git clone -q https://github.com/dotcloud/docker-registry.git
+cd docker-registry
+
+# Get dependencies
+pip install -q -r requirements.txt
+pip install -q -r test-requirements.txt
+pip install -q tox
+
+# Run registry tests
+tox || exit 1
+export PYTHONPATH=$(pwd)/docker-registry
+python -m unittest discover -p s3.py -s test || exit 1
+python -m unittest discover -p workflow.py -s test
+
